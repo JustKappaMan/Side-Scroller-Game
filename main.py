3e7119ac039e1070928971d599836856b912c4f2
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-09 15:16:34 +0500

    A basic game window that can be closed

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import sys
+
 import pygame
 
 
 def main():
-    print('Hello, World!')
+    pygame.init()
+    screen = pygame.display.set_mode((720, 480))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
 
 
 if __name__ == '__main__':
