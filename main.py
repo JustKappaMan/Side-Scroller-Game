64ed2c096b078c11af65c0c7edfb0af76c5b19db
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-09 16:58:58 +0500

    Added a function for placing an img randomly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sys import exit
 from math import ceil
 from pathlib import Path
+from random import randint
 
 import pygame
 
@@ -19,6 +20,16 @@ def tile_background(screen: pygame.display, image: pygame.Surface) -> None:
             screen.blit(image, (x * image_width, y * image_height))
 
 
+def place_image_randomly(screen: pygame.display, image: pygame.Surface) -> None:
+    screen_width, screen_height = screen.get_size()
+    image_width, image_height = image.get_size()
+
+    screen.blit(image, (
+        randint(SCREEN_PADDING, screen_width - SCREEN_PADDING - image_width // 2),
+        randint(SCREEN_PADDING, screen_height - SCREEN_PADDING - image_height // 2)
+    ))
+
+
 def main():
     pygame.init()
     pygame.display.set_caption('Simple Game')
@@ -27,6 +38,9 @@ def main():
 
     grass_surface = pygame.image.load(Path('graphics', 'grass.png'))
     tile_background(screen, grass_surface)
+
+    cherry = pygame.transform.scale(pygame.image.load(Path('graphics', 'cherry.png')), (50, 50))
+    place_image_randomly(screen, cherry)
 
     while True:
         for event in pygame.event.get():
