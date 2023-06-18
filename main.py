f2b9fd3fc44cd449c9171f3228ae071ff2dd90b1
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-18 13:05:26 +0500

    Improved place method of Sprite class
    It now takes into account screen size, screen padding, etc.

--- main.py
+++ main.py
@@ -27,6 +27,21 @@ class Sprite:
         self.y_position = None
 
     def place(self, screen: pg.display, x_position: int, y_position: int):
+        screen_width, screen_height = screen.get_size()
+        image_width, image_height = self.image.get_size()
+
+        min_x_position = SCREEN_PADDING
+        max_x_position = screen_width - SCREEN_PADDING - image_width // 2
+
+        if x_position < min_x_position or x_position > max_x_position:
+            raise ValueError(f'{x_position=}, correct values are {min_x_position}...{max_x_position}')
+
+        min_y_position = SCREEN_PADDING
+        max_y_position = screen_height - SCREEN_PADDING - image_height // 2
+
+        if y_position < min_y_position or y_position > max_y_position:
+            raise ValueError(f'{y_position=}, correct values are {min_y_position}...{max_y_position}')
+
         self.x_position, self.y_position = x_position, y_position
         screen.blit(self.image, (self.x_position, self.y_position))
 
@@ -59,9 +74,14 @@ def main():
                 exit()
             if event.type == pg.KEYDOWN:
                 tile_background(screen, grass)
-                if event.key == pg.K_a:
+                if event.key == pg.K_q:
+                    cherry.place(screen, -42, -322)
+                if event.key == pg.K_w:
+                    cherry.place(screen, 100, 100)
+                    print(f'Setting a cherry position to x={cherry.x_position}, y={cherry.y_position}')
+                if event.key == pg.K_e:
                     cherry.place_randomly(screen)
-                    print(f'Randomly setting a cherry position to x={cherry.x_position=}, y={cherry.y_position=}')
+                    print(f'Randomly setting a cherry position to x={cherry.x_position}, y={cherry.y_position}')
 
         pg.display.update()
         clock.tick(FRAMERATE_LIMIT)
