7972158e2a47dcb5e8a2e99a3fccf8b0626892f5
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-23 14:30:31 +0500

    Added sky

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ def main():
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pg.time.Clock()
 
+    sky_surface = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    sky_surface.fill('skyblue')
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+
+        screen.blit(sky_surface, (0, 0))
 
         pg.display.update()
         clock.tick(MAX_FRAMERATE)
