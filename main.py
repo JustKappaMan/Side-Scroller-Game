8aa29dc947102fb33f0ff54d26ef761fd01135ee
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-23 14:45:08 +0500

    Added ground

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     sky_surface = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     sky_surface.fill('skyblue')
 
+    ground_surface = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT // 4))
+    ground_surface.fill('tan4')
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -21,6 +24,7 @@ def main():
                 sys.exit()
 
         screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, SCREEN_HEIGHT - SCREEN_HEIGHT // 4))
 
         pg.display.update()
         clock.tick(MAX_FRAMERATE)
