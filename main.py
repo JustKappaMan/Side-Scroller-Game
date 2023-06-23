55be255df94ed1fcd461732cc09f9b0f1c2902ed
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-23 15:39:27 +0500

    Added a function to draw a ground image
    Height of the ground depends on a screen resolution

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import sys
+import math
+import pathlib
 
 import pygame as pg
 
 from settings import *
+
+
+def draw_ground(screen: pg.Surface, ground_surface: pg.Surface) -> None:
+    screen_width, screen_height = screen.get_size()
+    ground_width, ground_height = ground_surface.get_size()
+
+    tiles_count = math.ceil(screen_width / ground_width)
+
+    match round(screen_height / ground_height):
+        case 2:
+            ground_y = screen_height - ground_height // 4
+        case 3:
+            ground_y = screen_height - ground_height // 2
+        case _:
+            ground_y = screen_height - ground_height
+
+    for i in range(tiles_count):
+        screen.blit(ground_surface, (i * ground_width, ground_y))
 
 
 def main():
@@ -14,8 +34,8 @@ def main():
     sky_surface = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     sky_surface.fill('skyblue')
 
-    ground_surface = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT // 4))
-    ground_surface.fill('tan4')
+    ground_surface = pg.image.load(pathlib.Path('graphics', 'ground.png')).convert()
+    draw_ground(screen, ground_surface)
 
     while True:
         for event in pg.event.get():
@@ -24,7 +44,7 @@ def main():
                 sys.exit()
 
         screen.blit(sky_surface, (0, 0))
-        screen.blit(ground_surface, (0, SCREEN_HEIGHT - SCREEN_HEIGHT // 4))
+        draw_ground(screen, ground_surface)
 
         pg.display.update()
         clock.tick(MAX_FRAMERATE)
