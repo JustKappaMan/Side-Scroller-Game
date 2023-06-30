f6e2984d1ae59d8c08d1130018b3feb01c0c5c7b
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-06-30 16:29:34 +0500

    Refactor ScoreCounter class

--- main.py
+++ main.py
@@ -58,18 +58,19 @@ class ScoreCounter:
         self.color = pg.color.Color('darkgreen')
         self.position = (screen.get_width() - 8, 8)
         self.start_score = 0
-        self.score = None
+        self.current_score = None
+        self.score_divisor = 100
         self.surf = None
         self.rect = None
 
     def render(self):
-        self.score = pg.time.get_ticks() // 100 - self.start_score
-        self.surf = self.font.render(f'{self.score}', True, self.color)
+        self.current_score = pg.time.get_ticks() // self.score_divisor - self.start_score
+        self.surf = self.font.render(f'{self.current_score}', True, self.color)
         self.rect = self.surf.get_rect(topright=self.position)
         self.screen.blit(self.surf, self.rect)
 
     def refresh(self):
-        self.start_score = pg.time.get_ticks() // 100
+        self.start_score = pg.time.get_ticks() // self.score_divisor
 
 
 class GameOverScreen:
